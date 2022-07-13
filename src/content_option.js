88931e8f75f0e82659732268b190236e921b830f
imadlite <imaddjeddai@gmail.com>
2022-07-13 16:24:35 +0200

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,226 @@
+const logotext = "IMAD";
+const meta = {
+  title: "Imad Djeddai",
+  description:
+    "I’m Imad Djeddai data scientist _ Full stack devloper,currently working FreeLancer"
+};
+
+const introdata = {
+  title: "I’m Imad Djeddai",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+    four: "I develop system apps"
+  },
+  description:
+    "I am a hard working, honest individual. I am a good timekeeper, always willing to learn new skills. I am friendly, helpful and polite, have a good sense of humour. I am able to work independently in busy environments and also within a team setting. I am outgoing and tactful, and able to listen effectively when solving problems.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d"
+};
+
+const dataabout = {
+  title: "a bit about my self",
+  aboutme:
+    "I am a hard working, honest individual. I am a good timekeeper, always willing to learn new skills. I am friendly, helpful and polite, have a good sense of humour. I am able to work independently in busy environments and also within a team setting. I am outgoing and tactful, and able to listen effectively when solving problems."
+};
+const worktimeline = [
+  {
+    jobtitle: "Flutter developer",
+    where: "Algeria-Tebessa",
+    date: "2018 to now"
+  },
+  {
+    jobtitle: "Software Developer",
+    where: "Algeria-Tebessa",
+    date: "2015 to now"
+  },
+  {
+    jobtitle: "System Information",
+    where: "Algeria-Tebessa",
+    date: "2019 to now"
+  }
+];
+
+const skills = [
+  {
+    name: "Python (Tensorflow/Keras ...)",
+    value: 90
+  },
+  {
+    name: "Flutter (Android/Ios/Windows/Web)",
+    value: 90
+  },
+  {
+    name: "NodeJs (Server)",
+    value: 80
+  },
+  {
+    name: "Reactjs (Website)",
+    value: 50
+  },
+  {
+    name: "Java (Desktop/Mobile)",
+    value: 85
+  },
+  {
+    name: "C/C++",
+    value: 70
+  },
+  {
+    name: "Linux System",
+    value: 85
+  },{
+    name: "Microsoft Office",
+    value: 90
+  },{
+    name: "PHP (Server)",
+    value: 75
+  },{
+    name: "Matlab",
+    value: 75
+  }
+];
+
+const services = [
+  // {
+  //   title: "UI & UX Design",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin."
+  // },
+  {
+    title: "Mobile Apps",
+    description:
+      "this field is my favorite because flutter is a lover of all devs."
+  },
+  {
+    title: "Computer Vision (AI)",
+    description:
+      "in artificial intellegent we have some project with deep learning and machine learning."
+  },
+  {
+    title: "System Information",
+    description: "is my majority."
+  }
+];
+const language = [
+  {
+    name: "arabic",
+    value: 100
+  },
+  {
+    name: "Frensh",
+    value: 85
+  },
+  {
+    name: "English",
+    value: 80
+  }
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#"
+  }
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "imaddjeddai@gmail.com",
+  YOUR_FONE: "(213)666-7306-10",
+  description:
+    "I’m Imad Djeddai data scientist _ Full stack devloper,currently working FreeLancer. ",
+  YOUR_SERVICE_ID: "service_faflqd3",
+  YOUR_TEMPLATE_ID: "template_5n4crx9",
+  YOUR_USER_ID: "WCNLAvOgY0iHSFU3m"
+};
+
+const socialprofils = {
+  github: "https://github.com",
+  facebook: "https://facebook.com/imad.tchi.9",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
+  youtube: "https://youtube.com",
+  twitch: "https://twitch.com"
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  language,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext
+};
